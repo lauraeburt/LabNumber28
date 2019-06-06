b5ed1494b80412107522617b1eef1286ff3a8024
King <kingl@kndmba-1272.local>
2019-06-06 05:55:12 -0400

    initial commit

---
+++ src/main/java/com/LabNumber28/LabNumber28Burt/LabNumber28BurtApplication.java
@@ -0,0 +1,13 @@
+package com.LabNumber28.LabNumber28Burt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabNumber28BurtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabNumber28BurtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/LabNumber28/LabNumber28Burt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.LabNumber28.LabNumber28Burt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LabNumber28BurtApplication.class);
+	}
+
+}
